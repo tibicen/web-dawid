cee909e829824aa89af343e909b6cbc95d8dde38
Dawid Huczynski <dawid.huczynski@pm.me>
2022-03-31 22:57:53 +0200

    fix shadows, optimisation, layout

--- anna/js/main.js
+++ anna/js/main.js
@@ -4,50 +4,50 @@ import { OrbitControls } from "./OrbitControls.js";
 import { SSRPass } from "./SSRPass.js";
 import { EffectComposer } from './EffectComposer.js';
 
-let scene, camera, renderer, controls, model, hemilight, light, composer, ssrPass, podest;
+let scene, camera, renderer, controls, model, hemilight, light, composer, ssrPass, podest, spotlight;
 
 function init() {
     scene = new THREE.Scene();
     scene.background = new THREE.Color("#fafafa");
-    scene.fog = new THREE.FogExp2( "#fafafa", .04 );
+    scene.fog = new THREE.FogExp2("#fafafa", .04);
     // scene.fog = new THREE.Fog( "#fafafa", 5, 50 );
     renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.physicallyCorrectLights = true;
     renderer.toneMapping = THREE.ACESFilmicToneMapping;
     // renderer.toneMapping = THREE.ReinhardToneMapping;
     renderer.toneMappingExposure = 2.4;
     renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap; 
+    renderer.shadowMap.type = THREE.PCFShadowMap;
     // document.body.appendChild( renderer.domElement );
     document.getElementById('model').appendChild(renderer.domElement);
-    camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.set(0, 1, 5);
-    controls = new OrbitControls( camera, renderer.domElement );
-    controls.target = new THREE.Vector3(0,1,0);
-    controls.maxAzimuthAngle = 1;
-    controls.minAzimuthAngle = -1;
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.target = new THREE.Vector3(0, 1, 0);
+    // controls.maxAzimuthAngle = 1;
+    // controls.minAzimuthAngle = -1;
     controls.minDistance = 1;
     controls.maxDistance = 5.5;
-    controls.maxPolarAngle = 3;
+    controls.maxPolarAngle = 1.75;
     controls.minPolarAngle = 0;
     controls.enableDamping = true;
     controls.enablePan = false;
     controls.dampingFactor = .05;
     controls.update();
-    camera.lookAt(new THREE.Vector3(0,1,0));
+    camera.lookAt(new THREE.Vector3(0, 1, 0));
 
 
     new GLTFLoader().load("./models/ubranie.glb", result => {
         let models = result.scene.children;
         // camera = result.cameras[0];
-        console.log(result.scene);
-        result.scene.traverse( function( node ) {
-            if ( node.isMesh ) {
+        // console.log(result.scene);
+        result.scene.traverse(function (node) {
+            if (node.isMesh) {
                 node.castShadow = true;
                 node.receiveShadow = true;
             }
-            if( node.material && node.material.map ) node.material.map.anisotropy = 16;
+            if (node.material && node.material.map) node.material.map.anisotropy = 16;
             if (node.name == 'podest') podest = node;
         });
         // models.forEach(element => {
@@ -68,40 +68,32 @@ function init() {
 
     let factor = .5
 
-    // hemilight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 10); // soft white light
-    hemilight = new THREE.HemisphereLight("#ffffff", "#ffffff", 2*factor); // soft white light
-    scene.add( hemilight );
+    hemilight = new THREE.HemisphereLight("#ffffff", "#ffffff", 1);
+    scene.add(hemilight);
+
+    let lights = [
+        {power: 10, x: -3, y: 3, z: 0},
+        {power: 20, x: 1, y: 3, z: 4,}
+    ]
+    lights.forEach(params => {
+        spotlight = new THREE.SpotLight("#ffffff", params.power);
+        spotlight.position.set(params.x, params.y, params.z);
+        spotlight.castShadow = true;
+        spotlight.angle = .7;
+        spotlight.penumbra = 1;
+        spotlight.shadow.bias = -0.001;
+        spotlight.shadow.radius = 4;
+        // spotlight.decay = 1
+        spotlight.shadow.mapSize.width = 1024;
+        spotlight.shadow.mapSize.height = 1024;
+        spotlight.shadow.camera.near = 3;
+        spotlight.shadow.camera.far = 10;
+        spotlight.shadow.camera.fov = 30;
+        scene.add(spotlight)
+
+    });
 
 
-    let spotlight = new THREE.SpotLight("#ffffff", 20*factor);
-    spotlight.position.set( -3, 3, 0 );
-    spotlight.castShadow = true;
-    spotlight.angle = .7
-    spotlight.penumbra = 1
-    spotlight.radius = 5
-    spotlight.shadow.mapSize.width = 1024*4;
-    spotlight.shadow.mapSize.height = 1024*4;
-    // spotlight.decay =1
-    spotlight.shadow.camera.near = .1;
-    spotlight.shadow.camera.far = 10;
-    spotlight.shadow.camera.fov = 30;
-    scene.add(spotlight)
-
-    spotlight = new THREE.SpotLight("#ffffff", 40*factor);
-    spotlight.position.set( 1, 3, 4 );
-    spotlight.castShadow = true;
-    spotlight.angle = .7
-    spotlight.penumbra = 1
-    spotlight.shadow.radius = 5.0
-    // spotlight.decay =.5
-    spotlight.shadow.mapSize.width = 1024*4;
-    spotlight.shadow.mapSize.height = 1024*4;
-    spotlight.shadow.camera.near = 1;
-    spotlight.shadow.camera.far = 10;
-    spotlight.shadow.camera.fov = 30;
-    // spotlight.shadow.radius = 10.0;
-    // spotlight.shadow.focus = 1;
-    scene.add(spotlight)
     // light = new THREE.DirectionalLight("#ffffff", 4);
     // light.castShadow = true;
     // light.shadow.bias = -0.0001;
@@ -114,27 +106,21 @@ function init() {
 
     // composer = new EffectComposer( renderer );
 
-    podest = scene.getObjectByName( "podest" );
+    podest = scene.getObjectByName("podest");
 
     animate();
 }
 
 function animate() {
+    let focus_point = new THREE.Vector3(0, 1, 0);
     // composer.render();
-    renderer.render( scene,camera );
-    if (podest) {
-        podest.rotation.y += 0.002;
-    }
-
-	// spotlight.position.set(
-    //     camera.position.x + 10,
-    //     camera.position.y + 10,
-    //     camera.position.z + 10,
-    //     )
-    requestAnimationFrame( animate );
-
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+    camera.position.sub(focus_point);
+    camera.position.applyAxisAngle(new THREE.Vector3(0,1,0), .002);
+    camera.position.add(focus_point);
+    camera.lookAt(focus_point);
+    // camera.rotateOnAxis(new THREE.Vector3(0,1,0), .002);
+    requestAnimationFrame(animate);
 }
 init();
 
